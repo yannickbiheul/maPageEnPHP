979a317f3f6233b8818aca194595d632ffbdc0ae
Yannick Biheul <yannickbiheul@outlook.fr>
2020-04-28 10:52:25 +0200

    Début de rédaction section Présentation

--- presentation.php
+++ presentation.php
@@ -7,8 +7,25 @@
     <section id="presentation">
     
         <div class="container-presentation">
-            <h2>Chantier en cours</h2>
-            <img src="images/travaux.png">
+            <div class="carte-presentation">
+                <div class="presentation-titre">
+                    <h2>Qui suis-je</h2>
+                </div>
+                <div class="presentation-texte">
+                    <p>Je m'appelle Yannick Biheul, j'ai 38 ans et je vis à Quimper dans le Finistère (29).</p>
+                    <p>Je suis titulaire d'un BAC PRO Maintenance en Electroménager et d'un BEP Vente, et je travaille en tant qu'opérateur de production depuis 2003 chez un équipementier automobile.</p>
+                    <p>Je suis intéressé par les nouvelles technologies, les jeux vidéo, le cinéma, la lecture et la musique.</p>
+                </div>
+            </div>
+            <div class="carte-presentation2">
+            <div class="presentation-titre">
+                    <h2>Reconversion</h2>
+                </div>
+                <div class="presentation-texte">
+                    <p>Fin 2018, je décide de me renseigner sur une reconversion professionnelle et de viser un métier qui m'intéresse et qui me permette d'avoir des horaires normaux.</p>
+                    
+                </div>
+            </div>
         </div>
 
     </section>
